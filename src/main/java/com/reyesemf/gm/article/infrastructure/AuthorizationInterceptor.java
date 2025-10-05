0fef9cd92986f635909b8ff41f51dcd264a809fc
git-reyesemf <reyesemf@gmail.com>
2025-10-05 02:37:20 -0300

    log

--- src/main/java/com/reyesemf/gm/article/infrastructure/AuthorizationInterceptor.java
+++ src/main/java/com/reyesemf/gm/article/infrastructure/AuthorizationInterceptor.java
@@ -3,6 +3,7 @@ package com.reyesemf.gm.article.infrastructure;
 import com.reyesemf.gm.article.domain.service.AuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
@@ -10,9 +11,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+
+
 import static com.reyesemf.gm.article.domain.model.ActionName.LOGIN;
 import static java.lang.Boolean.TRUE;
 import static java.util.Objects.isNull;
+import static org.slf4j.LoggerFactory.getLogger;
+
 
 @Component
 public class AuthorizationInterceptor implements HandlerInterceptor {
@@ -22,16 +27,26 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
     @Autowired
     private AuthenticationService authenticationService;
 
+    private static final Logger LOG = getLogger(AuthorizationInterceptor.class);
+
     public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
         RequiredAction required = ((HandlerMethod) handler).getMethodAnnotation(RequiredAction.class);
 
+        LOG.info("authentication-service - 1");
+
         if (isNull(required)) {
+            LOG.info("authentication-service - 2");
             throw new InternalAuthenticationServiceException("Unhandled Action");
         }
 
+        LOG.info("authentication-service - 3");
+
         if (!LOGIN.equals(required.value())) {
+            LOG.info("authentication-service - 4");
             authenticationService.validate(request.getHeader(AUTH_TOKEN), required);
         }
+
+        LOG.info("authentication-service - 5");
 
         return TRUE;
 
